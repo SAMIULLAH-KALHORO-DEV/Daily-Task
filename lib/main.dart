02986229d26e2ecc473c205d74808bc3e75a8f10
SAMIULLAH-KALHORO-DEV <samiullahabbasy@gmail.com>
2023-05-23 01:33:27 +0500

    Design completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:design2/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
-
 void main(List<String> args) {
   runApp(MyApp());
 }
@@ -12,10 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // debugShowCheckedModeBanner: true,
       debugShowCheckedModeBanner: false,
-      
+
       home: WelcomeScreen(),
-      
     );
   }
 }
